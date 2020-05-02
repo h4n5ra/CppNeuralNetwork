569212ddae0acb7f32717dd027e50b10ba83558e
Harvir Hansra <h.hansra97@gmail.com>
2020-05-02 21:14:46 +0000

    Added Layer class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
-#include <iterator>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
-
-class Neuron 
-{
-private:
-    double m_bias;
-public:
-    Neuron() : m_bias(0) {}
-    Neuron(double& n) : m_bias(n) {} 
-    void test(){cout << this->m_bias << endl;}
-};
-
 
 double dot(vector<double> v1, vector<double> v2)
 {
@@ -40,16 +31,16 @@ vector<vector<double>> dot(vector<vector<double>> m1, vector<vector<double>> m2)
     return output;
 }
 
-vector<vector<double>> add(vector<vector<double>> m1, vector<double> v1)
+vector<vector<double>> add(vector<vector<double>> m, vector<double> v)
 {
-    for(int i = 0; i < size(m1); ++i)  // each row of m1
+    for(int i = 0; i < size(m); ++i)  // each row of m1
     {
-        for(int j = 0; j < size(m1[0]); ++j)
+        for(int j = 0; j < size(m[0]); ++j)
         {
-            m1[i][j] += v1[j];
+            m[i][j] += v[j];
         }
     }
-    return m1;
+    return m;
 }
 
 vector<vector<double>> transpose(vector<vector<double>> m)
@@ -65,7 +56,7 @@ vector<vector<double>> transpose(vector<vector<double>> m)
     return output;
 }
 
-void pp_matrix(vector<vector<double>> m)
+void pp(vector<vector<double>> m)
 {
     for(int i = 0; i < size(m); ++i)
     {
@@ -74,6 +65,14 @@ void pp_matrix(vector<vector<double>> m)
             cout << m[i][j] << "   ";
         }
         cout << '\n';
+    }
+}
+
+void pp(vector<double> v)
+{
+   for(int i = 0; i < size(v); ++i)
+    {
+        cout << v[i] << endl;
     }
 }
 
@@ -96,7 +95,7 @@ bool test_dot_matrix()
 
     vector<vector<double>> result = dot(m1, m2);
     return true;
-}
+} 
 
 bool test_add()
 {
@@ -137,10 +136,60 @@ bool test_transpose()
     return true;
 }
 
+class Layer_Dense
+{
+    private:
+        double m_inputs, m_neurons;
+        vector<double> m_biases;
+        vector<double> generate_biases();
+        vector<vector<double>> m_weights;
+        vector<vector<double>> generate_weights();
+
+    public:
+        Layer_Dense(int& inputs, int& neurons): m_inputs(inputs), m_neurons(neurons)
+        {
+            this->m_biases = this->generate_biases();
+            this->m_weights = this->generate_weights();
+        }
+
+        vector<double> getbiases() { return this->m_biases; };
+        vector<vector<double>> getweights() { return this->m_weights; };
+        vector<vector<double>> forward(vector<vector<double>>& input);
+};
+
+vector<vector<double>> Layer_Dense::generate_weights()
+{
+    srand(0);
+    // srand(time(NULL));
+    vector<vector<double>> weights(this->m_inputs);
+    for(int i = 0; i < this->m_inputs; ++i)
+    {
+        for(int j = 0; j < this->m_neurons; ++j)
+        {
+            weights[i].push_back(static_cast<double>(rand() % 100) / 1000);
+        }
+    } 
+    return weights;
+}
+
+vector<double> Layer_Dense::generate_biases()
+{
+    vector<double> biases(this->m_neurons, 0.0);
+    return biases;
+}
+
+vector<vector<double>> Layer_Dense::forward(vector<vector<double>>& input)
+{
+    vector<vector<double>> output;
+    output = add(dot(input, this->m_weights), this->m_biases);
+    return output;
+}
+
+
 int main()
 {
     // inputs from input layer or previous neuron layer output
-    vector<vector<double>> inputs = {
+    vector<vector<double>> X = {
         {1.0, 2.0, 3.0, 2.5},
         {2.0, 5.0, -1.0, 2.0},
         {-1.5, 2.7, 3.3, -0.8}
@@ -162,9 +211,20 @@ int main()
         {-0.44, 0.73, -0.13}
     };
 
-    vector<vector<double>> layer1 = add(dot(inputs, transpose(weights1)), bias1);
-    pp_matrix(add(dot(layer1, transpose(weights2)), bias2));
+    // vector<vector<double>> layer1 = add(dot(X, transpose(weights1)), bias1);
+    // pp(add(dot(layer1, transpose(weights2)), bias2));
 
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    Layer_Dense ld1 = Layer_Dense(a, b);
+    Layer_Dense ld2 = Layer_Dense(b, c);
+
+    vector<vector<double>> layer1_output = ld1.forward(X);
+    pp(layer1_output);
+    cout << '\n';
+    pp(ld2.forward(layer1_output));
+        
     return 0;
 
 }
